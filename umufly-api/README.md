a9a054e683338586e0ec5e82c55037b5560dab56
Juan Francisco Sánchez Peñalver <juanfrancisco.sanchez@ticarum.es>
2026-02-03 17:03:41 +0100

    CLOUD-2: Crear dominio UMUFly (#12)
    * CLOUD-2: Se crean endpoints de pruebas.
    
    * CLOUD-2: Se empieza a modelar el dominio.
    
    * CLOUD-2: Se añaden colecciones de ejemplo
    
    * CLOUD-2: Completada la lógica en el agregado raíz.
    
    * CLOUD-2: Se añade el application.properties
    
    * CLOUD-2: Se ajusta el pasajero para añadir documento de identidad.
    
    * CLOUD-2: Se corrige la imagen del modelado del dominio.
    
    * CLOUD-2: Se ajusta el modelo del dominio y se quitan eventos.
    
    * CLOUD-2: Se agrupa Itinerario y Ruta. También se modelan los VO como record.
    
    * CLOUD-2: Se añade estado PENDIENTE para la reserva de vuelo.
    
    * CLOUD-2: Se quitan los optional del dominio.
    
    * CLOUD-2: Se ajustan los métodos del agregado raíz.

--- umufly-api/README.md
+++ umufly-api/README.md
@@ -1,3 +1,4 @@
-# UMUFly API
-
-Proyecto backend del caso práctico **UMUFly**.
+## Aplicación umufly-api V.0.0.1-SNAPSHOT
+## API que ofrece operaciones de gestión de vuelos y reservas
+# Artefacto inicial:  es.um.atica.umufly-api-0.0.1-SNAPSHOT
+# Entrypoint inicial: es.um.atica.UmuflyApiApplication
---
+++ umufly-api/src/main/java/es/um/atica/umufly/UmuflyApiApplication.java
@@ -0,0 +1,15 @@
+package es.um.atica.umufly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication( scanBasePackages = {
+		"es.um.atica.umufly", "es.um.atica.fundewebjs"
+} )
+public class UmuflyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmuflyApiApplication.class, args);
+	}
+
+}
